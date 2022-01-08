b4a89b64c3fdeec59ac0a37c69668bf4fc82a535
angelorange <acostalarangeira@gmail.com>
2022-01-08 17:57:37 -0300

    feat: add documentation

--- lib/coin_web/router.ex
+++ lib/coin_web/router.ex
@@ -8,16 +8,9 @@ defmodule CoinWeb.Router do
   scope "/api", CoinWeb do
     pipe_through :api
 
-    resources "/users", UserController, only: [:update, :create, :delete, :index, :show]
+    resources "/users", UserController, except: [:new, :edit]
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
@@ -25,18 +18,6 @@ defmodule CoinWeb.Router do
       pipe_through [:fetch_session, :protect_from_forgery]
 
       live_dashboard "/dashboard", metrics: CoinWeb.Telemetry
-    end
-  end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
